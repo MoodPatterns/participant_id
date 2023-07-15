c206c0da2fb91e4e17d29884d5037f02c06eb696
Bernhard Piskernik <bpiskernik@novomatic.com>
2023-07-15 16:13:00 +0200

    wip: cards for start page

--- lib/pages/page_start.dart
+++ lib/pages/page_start.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:participant_id/utils/const/app_data.dart';
 import 'package:participant_id/widgets/page_body.dart';
@@ -14,6 +18,12 @@ class PageStart extends StatelessWidget {
 
     var layoutProperties = AppData.layoutProperties(mq.size.width);
 
+    var cardWidth = List<double>.from([
+      mq.size.width / 2,
+      mq.size.height / 2,
+      300 * sqrt(layoutProperties.textScalingFactor)
+    ]).reduce(min);
+
     return Scaffold(
         body: Padding(
       padding: EdgeInsets.all(layoutProperties.edgeInsets),
@@ -24,9 +34,73 @@ class PageStart extends StatelessWidget {
                 onPressed: () {},
                 child: Text(
                   'create_participant_id',
-                  style: theme.textTheme.displayMedium,
+                  style: theme.textTheme.displaySmall,
                   textScaleFactor: layoutProperties.textScalingFactor,
-                ).tr())
+                ).tr()),
+            SizedBox(height: layoutProperties.edgeInsets * 2),
+            Wrap(
+              spacing: layoutProperties.edgeInsets,
+              runSpacing: layoutProperties.edgeInsets,
+              alignment: WrapAlignment.center,
+              children: [
+                SizedBox(
+                  width: cardWidth,
+                  height: cardWidth * 1.34,
+                  child: Card(
+                      color: theme.primaryColor,
+                    child: FlipCard(
+                      front: Card(
+                        //color: theme.colorScheme.secondary,
+                        child: Padding(
+                          padding: EdgeInsets.all(layoutProperties.edgeInsets),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                               const Flexible(
+                                flex: 3,
+                                child: Expanded(
+                                  child: FittedBox(
+                                      fit: BoxFit.fill, child: Icon(Icons.shield)),
+                                ),
+                              ),
+                              Flexible(
+                                flex: 1,
+                                child: AutoSizeText(
+                                  'secure'.tr(),
+                                  style: theme.textTheme.headlineLarge,
+                                  maxFontSize:
+                                      theme.textTheme.headlineLarge!.fontSize! *
+                                          layoutProperties.textScalingFactor,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      back: Card(
+                        //color: theme.colorScheme.secondary,
+                        child: Center(
+                          child: RichText(
+                            text: TextSpan(
+                              text: 'Hello ',
+                              style: DefaultTextStyle.of(context).style,
+                              children: const <TextSpan>[
+                                TextSpan(
+                                    text: 'bold',
+                                    style: TextStyle(fontWeight: FontWeight.bold)),
+                                TextSpan(text: ' world!'),
+                              ],
+                            ),
+                            textScaleFactor: layoutProperties.textScalingFactor,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
