783e76efa0040f79439905c91c97383119856a70
Bernhard Piskernik <bpiskernik@novomatic.com>
2023-07-21 19:29:47 +0200

    fix   web hover on cards

--- lib/widgets/details_card.dart
+++ lib/widgets/details_card.dart
@@ -15,50 +15,53 @@ class DetailsCard extends StatelessWidget {
 
   const DetailsCard({super.key, required this.padding, required this.theme, required this.textScalingFactor, required this.icon, required this.header, required this.headerStyle, required this.explanation, required this.explanationStyle});
 
-
+  final MouseCursor cursor = SystemMouseCursors.click;
 
   @override
   Widget build(BuildContext context) {
     return Card(
     color: theme.primaryColor,
-    child: FlipCard(
-      front: Card(
-        //color: theme.colorScheme.secondary,
-        child: Padding(
-          padding: EdgeInsets.all(padding),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Flexible(
-                flex: 3,
-                  child: FittedBox(
-                      fit: BoxFit.fill, child: Icon(icon),),
+    child: MouseRegion(
+      cursor: cursor,
+      child: FlipCard(
+        front: Card(
+          //color: theme.colorScheme.secondary,
+          child: Padding(
+            padding: EdgeInsets.all(padding),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Flexible(
+                  flex: 3,
+                    child: FittedBox(
+                        fit: BoxFit.fill, child: Icon(icon),),
 
-              ),
-              Flexible(
-                flex: 1,
-                child: AutoSizeText(
-                  header,
-                  textScaleFactor: textScalingFactor,
-                  style: theme.textTheme.titleLarge,
-                  maxFontSize:
-                  theme.textTheme.titleLarge!.fontSize!,
-                  textAlign: TextAlign.center,
                 ),
-              )
-            ],
+                Flexible(
+                  flex: 1,
+                  child: AutoSizeText(
+                    header,
+                    textScaleFactor: textScalingFactor,
+                    style: theme.textTheme.titleLarge,
+                    maxFontSize:
+                    theme.textTheme.titleLarge!.fontSize!,
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-      back: Card(
-        child: Padding(
-          padding: EdgeInsets.all(padding),
-          child: SingleChildScrollView(
-            child: RichText(
-              text: explanation,
-              textScaleFactor: textScalingFactor,
+        back: Card(
+          child: Padding(
+            padding: EdgeInsets.all(padding),
+            child: SingleChildScrollView(
+              child: RichText(
+                text: explanation,
+                textScaleFactor: textScalingFactor,
+              ),
             ),
           ),
         ),
